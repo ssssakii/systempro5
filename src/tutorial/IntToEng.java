1dfe11469b54a956cc74fdf8dd7bd04618337d9c
g15948ts <g15948ts@s30408.tsuda.ac.jp>
2017-05-19 11:46:12 +0900

    driver:kana navigator:saki 二桁の数字（teen以外）

--- src/tutorial/IntToEng.java
+++ src/tutorial/IntToEng.java
@@ -15,18 +15,18 @@ public class IntToEng {
     // 数値を英訳する変換するメソッド
     static String translateEng(int n) {
     	if (n/10 == 0) {
-    		print1(n);
+    		return print1(n);
+    	}else if (n/10 !=0 ) {
+    		return print2(n)+" "+print1(n%10);
     	}
     	
     	return "";
     }
 
-    
-
 	static String print1 (int a) {
 		String no = "";
 		switch (a) {
-			case 0: break;
+			case 0: no = "zero";break;
 			case 1: no = "one"; break;
 			case 2: no = "two"; break;
 			case 3: no = "three"; break;
@@ -36,6 +36,23 @@ public class IntToEng {
 			case 7: no = "seven"; break;
 			case 8: no = "eight"; break;
 			case 9: no = "nine"; break;
+		}
+		return no;
+	}
+	
+	static String print2 (int a) {
+		String no = "";
+		switch (a/10) {
+			case 0: break;
+			case 1: no = "ten"; break;
+			case 2: no = "twenty"; break;
+			case 3: no = "thirty"; break;
+			case 4: no = "forty"; break;
+			case 5: no = "fifty"; break;
+			case 6: no = "sixty"; break;
+			case 7: no = "seventy"; break;
+			case 8: no = "eighty"; break;
+			case 9: no = "ninety"; break;
 		}
 		return no;
 	}
--- test/tutorial/test.java
+++ test/tutorial/test.java
@@ -1,20 +1,26 @@
 package tutorial;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
 
 import org.junit.Test;
 
 public class test {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-	
 	public void print1のテスト() {
 		IntToEng ite = new IntToEng();
-		String actual = ite.translateEng("two");
-		System.out.println(actual);
+		String expected="two";
+		String actual = ite.translateEng(2);
+		assertThat(actual,is(expected));
+	}
+	
+	@Test
+	public void print2のテスト() {
+		IntToEng ite = new IntToEng();
+		String expected="twenty one";
+		String actual = ite.translateEng(21);
+		assertThat(actual,is(expected));
 	}
 
 }
